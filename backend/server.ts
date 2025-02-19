585ab4926fcd74c82f26d8de3ad98a3014c96f7e
asmita <asmitapatil9904@gmail.com>
2025-02-19 16:26:19 +0530

    updated cors

--- backend/server.ts
+++ backend/server.ts
@@ -28,7 +28,11 @@ const PORT = process.env.PORT || 5000
 // MONGO_URI="mongodb+srv://asmita:1234@clothing-app.fi3cy.mongodb.net/clothing-app?retryWrites=true&w=majority&appName=clothing-app"
 // PORT=5000
 // JWT_SECRET="jwt"
-app.use(cors())
+app.use(cors({
+  origin:["https://ap-customevogue-mern.vercel.app/"],
+  methods:["POST","GET"],
+  credentials:true
+}))
 app.use(express.json())
 
 // Serve static files from the 'uploads' directory
